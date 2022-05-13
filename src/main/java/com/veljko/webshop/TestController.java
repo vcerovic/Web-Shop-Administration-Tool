cce143984f7b1097ba26478d7c041718b0c3494a
vcerovic <xvcerovic@gmail.com>
2022-05-13 18:53:06 +0200

    Basic project setup (controller, view)

---
+++ src/main/java/com/veljko/webshop/TestController.java
@@ -0,0 +1,16 @@
+package com.veljko.webshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String testHomePage(){
+
+        return "hello";
+    }
+}
